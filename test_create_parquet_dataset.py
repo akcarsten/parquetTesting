b5d03193e9fa4c46cb9f5d07e7b9d0d807c12515
carsten klein <carsten.a.klein@gmail.com>
2020-11-10 23:35:43 +0100

    added test to verify columns of parquet file

--- test_create_parquet_dataset.py
+++ test_create_parquet_dataset.py
@@ -9,6 +9,8 @@ class TestCreateParquetDataset(unittest.TestCase):
         def setUp(self):
             self.test_csv_file = 'data/btcusd.csv'
             self.test_parquet_file = 'data/test.parquet'
+            self.test_columns = ['time', 'open', 'close', 'high', 'low', 'volume']
+
 
         def tearDown(self):
             os.remove(self.test_parquet_file)
@@ -16,5 +18,11 @@ class TestCreateParquetDataset(unittest.TestCase):
         def test_if_parquet_file_is_created(self):
 
                 create_parquet_dataset(self.test_csv_file)
-
                 self.assertTrue(os.path.isfile(self.test_parquet_file))
+
+        def test_if_parquet_file_contains_all_columns(self):
+
+            create_parquet_dataset(self.test_csv_file)
+            df = pq.read_pandas(self.test_parquet_file).to_pandas()
+
+            self.assertListEqual(df.columns.tolist(), self.test_columns)
