16482637c8de30509f000fb4f949d38c949a75e9
carsten klein <carsten.a.klein@gmail.com>
2020-11-10 23:44:59 +0100

    added output file as an input argument

--- create_parquet_dataset.py
+++ create_parquet_dataset.py
@@ -3,9 +3,9 @@ import pyarrow.parquet as pq
 import pandas as pd
 
 
-def create_parquet_dataset(input_file):
+def create_parquet_dataset(input_file, output_file):
 
     df = pd.read_csv(input_file)
 
     table = pa.Table.from_pandas(df)
-    pq.write_table(table, 'data/test.parquet')
+    pq.write_table(table, output_file)
--- test_create_parquet_dataset.py
+++ test_create_parquet_dataset.py
@@ -7,22 +7,29 @@ from create_parquet_dataset import *
 class TestCreateParquetDataset(unittest.TestCase):
 
         def setUp(self):
-            self.test_csv_file = 'data/btcusd.csv'
-            self.test_parquet_file = 'data/test.parquet'
+            self.input_file = 'data/btcusd.csv'
+            self.output_file = 'data/test.parquet'
             self.test_columns = ['time', 'open', 'close', 'high', 'low', 'volume']
 
 
         def tearDown(self):
-            os.remove(self.test_parquet_file)
+            os.remove(self.output_file)
 
         def test_if_parquet_file_is_created(self):
 
-                create_parquet_dataset(self.test_csv_file)
-                self.assertTrue(os.path.isfile(self.test_parquet_file))
+                create_parquet_dataset(self.input_file, self.output_file)
+                self.assertTrue(os.path.isfile(self.output_file))
 
         def test_if_parquet_file_contains_all_columns(self):
 
-            create_parquet_dataset(self.test_csv_file)
-            df = pq.read_pandas(self.test_parquet_file).to_pandas()
+            create_parquet_dataset(self.input_file, self.output_file)
 
+            df = pq.read_pandas(self.output_file).to_pandas()
             self.assertListEqual(df.columns.tolist(), self.test_columns)
+
+        def test_meta_data_of_parquet_file(self):
+
+            create_parquet_dataset(self.input_file, self.output_file)
+
+            parquet_file = pq.ParquetFile(self.output_file)
+            print(parquet_file.metadata)
